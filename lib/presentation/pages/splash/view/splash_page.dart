c607a326e3e5b99da827c95993fa5b75e7b7ae61
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-07-06 18:36:38 +0200

    style: Remove unused imports in splash page

--- lib/presentation/pages/splash/view/splash_page.dart
+++ lib/presentation/pages/splash/view/splash_page.dart
@@ -1,7 +1,4 @@
-import 'dart:developer';
-
 import 'package:auto_route/auto_route.dart';
-import 'package:choose_app/core/core.dart';
 import 'package:choose_app/presentation/pages/splash/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
