1a4f57c2bca0a9d3e541001430d97864d32cf034
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-07-13 08:37:22 +0200

    feat: Update home bloc

--- lib/presentation/pages/home/bloc/home_bloc.dart
+++ lib/presentation/pages/home/bloc/home_bloc.dart
@@ -1,14 +1,71 @@
 import 'package:bloc/bloc.dart';
+import 'package:choose_app/domain/model/choices/choice_entity.dart';
+import 'package:choose_app/domain/use_case/draw_choice_use_case.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 part 'home_bloc.freezed.dart';
 
+@injectable
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(const HomeState.initial()) {
+  HomeBloc(this._drawChoiceUseCase) : super(const HomeState.success()) {
     on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
+      event.when(
+        choiceAdded: (choice) => _onChoiceAdded(choice, emit),
+        choiceRemoved: (choice) => _onChoiceRemoved(choice, emit),
+        choicesSubmitted: () => _onChoicesSubmitted(emit),
+      );
     });
+  }
+
+  final DrawChoiceUseCase _drawChoiceUseCase;
+
+  Future<void> _onChoiceAdded(
+    ChoiceEntity choice,
+    Emitter<HomeState> emit,
+  ) async {
+    if (state is! HomeSuccessState) return;
+
+    final successState = state as HomeSuccessState;
+
+    emit(
+      successState.copyWith(userChoices: [...successState.userChoices, choice]),
+    );
+  }
+
+  Future<void> _onChoiceRemoved(
+    ChoiceEntity removedChoice,
+    Emitter<HomeState> emit,
+  ) async {
+    if (state is! HomeSuccessState) return;
+
+    final successState = state as HomeSuccessState;
+
+    final filteredChoices = successState.userChoices
+        .where((choice) => choice != removedChoice)
+        .toList();
+
+    emit(
+      successState.copyWith(userChoices: filteredChoices),
+    );
+  }
+
+  Future<void> _onChoicesSubmitted(
+    Emitter<HomeState> emit,
+  ) async {
+    if (state is! HomeSuccessState) return;
+
+    final successState = state as HomeSuccessState;
+
+    final choiceResult = await _drawChoiceUseCase(successState.userChoices);
+
+    choiceResult.fold(
+      (error) => emit(const HomeState.error()),
+      (choice) => emit(
+        successState.copyWith(selectedChoice: choice),
+      ),
+    );
   }
 }
