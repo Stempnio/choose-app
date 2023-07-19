07fa3082022d0990259743036fb044ed064cb196
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-07-19 16:48:52 +0200

    test: Update choices repository tests

--- test/domain/repository/choices_data.dart
+++ test/domain/repository/choices_data.dart
@@ -1,16 +1,25 @@
 import 'package:choose_app/domain/model/choices/choices.dart';
+import 'package:uuid/uuid.dart';
 
-const choice1 = ChoiceEntity(
+final choice1 = ChoiceEntity(
   name: 'Cinema',
   type: ChoiceType.place,
-);
-const choice2 = ChoiceEntity(
-  name: 'Park',
-  type: ChoiceType.place,
-);
-const choice3 = ChoiceEntity(
-  name: 'Swimming',
-  type: ChoiceType.activity,
+  instanceId: const Uuid().v4(),
+  id: const Uuid().v4(),
 );
 
-const choicesList = [choice1, choice2, choice3];
+final choice2 = ChoiceEntity(
+  name: 'Park',
+  type: ChoiceType.place,
+  instanceId: const Uuid().v4(),
+  id: const Uuid().v4(),
+);
+
+final choice3 = ChoiceEntity(
+  name: 'Swimming',
+  type: ChoiceType.activity,
+  instanceId: const Uuid().v4(),
+  id: const Uuid().v4(),
+);
+
+final choicesList = [choice1, choice2, choice3];
--- test/domain/repository/choices_repository_test.dart
+++ test/domain/repository/choices_repository_test.dart
@@ -1,4 +1,7 @@
 import 'package:choose_app/data/data.dart';
+import 'package:choose_app/domain/domain.dart';
+import 'package:choose_app/domain/model/choices/choice_entity.dart';
+import 'package:dartz/dartz.dart';
 import 'package:test/test.dart';
 import 'choices_data.dart';
 
@@ -6,16 +9,23 @@ void main() {
   final choicesRepository = ChoicesRepositoryImpl();
   group('Choices Repository', () {
     test('drawChoice() returns a random choice from the list', () async {
-      final choice = await choicesRepository.drawChoice(choicesList);
+      final result = await choicesRepository.drawChoice(choicesList);
 
-      expect(choicesList.contains(choice), true);
+      expect(
+        result.fold(
+          (l) => false,
+          choicesList.contains,
+        ),
+        equals(true),
+      );
     });
 
-    test('drawChoice() throws NoChoicesGivenException when the list is empty',
-        () async {
+    test('drawChoice() returns Left when the list is empty', () async {
+      final result = await choicesRepository.drawChoice([]);
+
       expect(
-        choicesRepository.drawChoice([]),
-        throwsA(isA<NoChoicesGivenException>()),
+        result,
+        isA<Left<AppError, ChoiceEntity>>(),
       );
     });
   });
