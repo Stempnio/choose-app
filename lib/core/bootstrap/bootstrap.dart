a494abfe690a182f55c291e94d26d34a53369063
Jakub Stępień <jakub.stepien2905@gmail.com>
2023-07-20 09:12:46 +0200

    feat: Add package for handling .env

--- lib/core/bootstrap/bootstrap.dart
+++ lib/core/bootstrap/bootstrap.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'dart:developer';
-
 import 'package:bloc/bloc.dart';
 import 'package:choose_app/core/core.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:injectable/injectable.dart';
 
@@ -20,6 +20,8 @@ Future<void> bootstrap(
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   Bloc.observer = const AppBlocObserver();
+
+  await dotenv.load();
 
   configureDependencies(env);
 
